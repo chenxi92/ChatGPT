b1ed0106c5e6650be6fab20795a9ad6851bebf33
peak <peak@jiemogame.com>
2023-02-21 16:06:10 +0800

    change size on create dmg

--- scripts/create-dmg.sh
+++ scripts/create-dmg.sh
@@ -16,7 +16,7 @@ test -d ./build/Release/${AppName}.app || exit 1
 # if the target dmg file exist, remove it
 test -f ${AppName}.dmg && rm ${AppName}.dmg
 
-hdiutil create -megabytes 54 -fs HFS+ -volname ${VolumeName} ${AppName}.dmg
+hdiutil create -megabytes 2.5 -fs HFS+ -volname ${VolumeName} ${AppName}.dmg
 hdiutil mount ${AppName}.dmg
 
 cp -r ./build/Release/${AppName}.app /Volumes/${VolumeName}
